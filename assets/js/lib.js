809e4a3c9c49b34c52f3308ba9a4ba6593b9a5b7
Alberto Piras <a.piras@entando.com>
2016-03-29 15:43:45 +0200

    adding first html + js

---
+++ assets/js/lib.js
@@ -0,0 +1,88 @@
+ var app = angular.module('autocompleteApp', ['']);
+
+
+ app.controller('autocomplete', ['$scope', function('$scope'){
+
+
+     $scope.services= [{id:"first",value="primo elemento"},
+     {id:"second",value="secondo elemento"},
+     {id:"third",value="terzo elemento"}];
+
+     /***AUTOCOMPLETER***/
+     /*Autocompleter start*/
+
+     $scope.cleanAddService = function(){
+        $scope.enableAddService = false;
+        $scope.serviceSelected = "";
+    };
+
+    $scope.enableAddService = false;
+    $scope.serviceSelected;
+
+    $scope.addSelectItem = function(serviceId){
+        $scope.searchService =  $scope.getServiceName(serviceId);
+        $timeout(function(){
+            $scope.serviceSelected=serviceId;
+            angular.element(".autocomplete_select").addClass("hideElement");
+            $scope.enableAddService = true;
+        },100);
+    };
+
+    $scope.$watch('searchService', function(newValue, oldValue, scope) {
+        if(newValue !== oldValue){
+            $scope.cleanAddService();
+            angular.element(".autocomplete_select").removeClass("hideElement");
+        }
+    });
+
+    angular.element('body').click(function() {
+        angular.element(".autocomplete_select").addClass("hideElement");
+        $scope.cleanAddService();
+    });
+
+    window.displayBoxIndex = -1;
+
+    $("#searchService").keyup(function(e) 
+    {
+        if (e.keyCode == 40) 
+        {  
+            Navigate(1);
+        }
+        if(e.keyCode==38)
+        {
+            Navigate(-1);
+        }
+        if(e.keyCode==13)
+        {
+            selectListItem();
+        }
+    });
+
+    var Navigate = function(diff) {
+        displayBoxIndex += diff;
+        var oBoxCollection = $(".display_box");
+        if (displayBoxIndex >= oBoxCollection.length)
+            displayBoxIndex = 0;
+        if (displayBoxIndex < 0)
+            displayBoxIndex = oBoxCollection.length - 1;
+        var cssClass = "display_box_hover";
+        oBoxCollection.removeClass(cssClass).eq(displayBoxIndex).addClass(cssClass);
+    };
+
+    var selectListItem = function(diff) {
+        var oBoxCollection = $(".display_box");
+        var cssClass = "display_box_hover";
+        var temp = oBoxCollection.removeClass(cssClass).eq(displayBoxIndex).click();
+        
+    };
+
+    /*Autocompleter End*/
+
+    $scope.getServiceName = function(serviceId){
+        var service = _.find($scope.services, function(service){ return service.id === serviceId;});
+        var response = (service !== undefined)? service.id: serviceId;
+        return response;
+    };
+
+}]);
+
