50cddd08d4c186358139cfb8a7ac9c82d7b1ebf2
Alberto Piras <a.piras@entando.com>
2016-03-31 20:33:41 +0200

    addingu custom component scope to autocomplete directive

--- assets/js/lib.js
+++ assets/js/lib.js
@@ -1,93 +1,192 @@
-var app = angular.module('autocomplete',['ngRoute']);
 
 
+angular.module('autocompleteComponent',[])
+.directive('autocomplete',function(){
+    var componentAutocomplete;
+    return{
+        restrict : 'E',
+        scope:{
+         services: '=',
+     },
+     link: function(scope,element){
+                // selects current 'directive Element to ensure a custom search scope'
+                componentAutocomplete = element[0];
+                console.log(componentAutocomplete);
+            },
+            controller: function($scope,$timeout){
+               /***AUTOCOMPLETER***/
+               /*Autocompleter start*/
+
+               $scope.cleanAddService = function(){
+                $scope.enableAddService = false;
+                $scope.serviceSelected = "";
+            };
+
+            $scope.enableAddService = false;
+            $scope.serviceSelected;
+
+            $scope.addSelectItem = function(serviceId){
+                $scope.searchService =  $scope.getServiceName(serviceId);
+                $timeout(function(){
+                    $scope.serviceSelected=serviceId;
+                    angular.element(componentAutocomplete).find(".autocomplete_select").addClass("hideElement");
+                    $scope.enableAddService = true;
+                },100);
+            };
+
+            $scope.$watch('searchService', function(newValue, oldValue, scope) {
+                if(newValue !== oldValue){
+                    $scope.cleanAddService();
+                    console.log("watch");
+                    console.log(componentAutocomplete);
+                    angular.element(componentAutocomplete).find(".autocomplete_select").removeClass("hideElement");
+                    console.log(componentAutocomplete.querySelector(".autocomplete_select"));
+                }
+            });
+
+            angular.element('body').click(function() {
+                angular.element(".autocomplete_select").addClass("hideElement");
+                $scope.cleanAddService();
+            });
+
+            window.displayBoxIndex = -1;
+
+            $("#searchService").keyup(function(e) 
+            {
+                if (e.keyCode == 40) 
+                {  
+                    Navigate(1);
+                }
+                if(e.keyCode==38)
+                {
+                    Navigate(-1);
+                }
+                if(e.keyCode==13)
+                {
+                    selectListItem();
+                }
+            });
+
+            var Navigate = function(diff) {
+                displayBoxIndex += diff;
+                var oBoxCollection = angular.element(componentAutocomplete).find(".display_box");
+                if (displayBoxIndex >= oBoxCollection.length)
+                    displayBoxIndex = 0;
+                if (displayBoxIndex < 0)
+                    displayBoxIndex = oBoxCollection.length - 1;
+                var cssClass = "display_box_hover";
+                oBoxCollection.removeClass(cssClass).eq(displayBoxIndex).addClass(cssClass);
+            };
+
+            var selectListItem = function(diff) {
+                var oBoxCollection = angular.element(componentAutocomplete).find(".display_box");
+                var cssClass = "display_box_hover";
+                var temp = oBoxCollection.removeClass(cssClass).eq(displayBoxIndex).click();
+
+            };
+
+            /*Autocompleter End*/
+
+            $scope.getServiceName = function(serviceId){
+                var service = _.find($scope.services, function(service){ return service.id === serviceId;});
+                var response = (service !== undefined)? service.id: serviceId;
+                return response;
+            };
+        },  
+        templateUrl : 'autocomplete.html'
+
+    }
+});
+
+
+var app = angular.module('autocomplete',['ngRoute','autocompleteComponent']);
 
 
 app.config(function ($httpProvider) {
   delete $httpProvider.defaults.headers.common['X-Requested-With'];
 });
 
-app.controller('autocompleteController', function ($scope, $rootScope, $compile, $timeout) {
+app.controller('autocompleteController', function ($scope, $timeout) {
 
-     $scope.services= [{"id":"first","value":"primo elemento"},
-     {"id":"second","value":"secondo elemento"},
-     {"id":"third","value":"terzo elemento"}];
+   $scope.services= [{"id":"first","value":"primo elemento"},
+   {"id":"second","value":"secondo elemento"},
+   {"id":"third","value":"terzo elemento"}];
 
-     /***AUTOCOMPLETER***/
-     /*Autocompleter start*/
+   /***AUTOCOMPLETER***/
+   /*Autocompleter start*/
 
-     $scope.cleanAddService = function(){
-        $scope.enableAddService = false;
-        $scope.serviceSelected = "";
-    };
-
+   $scope.cleanAddService = function(){
     $scope.enableAddService = false;
-    $scope.serviceSelected;
+    $scope.serviceSelected = "";
+};
 
-    $scope.addSelectItem = function(serviceId){
-        $scope.searchService =  $scope.getServiceName(serviceId);
-        $timeout(function(){
-            $scope.serviceSelected=serviceId;
-            angular.element(".autocomplete_select").addClass("hideElement");
-            $scope.enableAddService = true;
-        },100);
-    };
+$scope.enableAddService = false;
+$scope.serviceSelected;
 
-    $scope.$watch('searchService', function(newValue, oldValue, scope) {
-        if(newValue !== oldValue){
-            $scope.cleanAddService();
-            angular.element(".autocomplete_select").removeClass("hideElement");
-        }
-    });
-
-    angular.element('body').click(function() {
+$scope.addSelectItem = function(serviceId){
+    $scope.searchService =  $scope.getServiceName(serviceId);
+    $timeout(function(){
+        $scope.serviceSelected=serviceId;
         angular.element(".autocomplete_select").addClass("hideElement");
+        $scope.enableAddService = true;
+    },100);
+};
+
+$scope.$watch('searchService', function(newValue, oldValue, scope) {
+    if(newValue !== oldValue){
         $scope.cleanAddService();
-    });
+        angular.element(".autocomplete_select").removeClass("hideElement");
+    }
+});
 
-    window.displayBoxIndex = -1;
+angular.element('body').click(function() {
+    angular.element(".autocomplete_select").addClass("hideElement");
+    $scope.cleanAddService();
+});
 
-    $("#searchService").keyup(function(e) 
+window.displayBoxIndex = -1;
+
+$("#searchService").keyup(function(e) 
+{
+    if (e.keyCode == 40) 
+    {  
+        Navigate(1);
+    }
+    if(e.keyCode==38)
     {
-        if (e.keyCode == 40) 
-        {  
-            Navigate(1);
-        }
-        if(e.keyCode==38)
-        {
-            Navigate(-1);
-        }
-        if(e.keyCode==13)
-        {
-            selectListItem();
-        }
-    });
+        Navigate(-1);
+    }
+    if(e.keyCode==13)
+    {
+        selectListItem();
+    }
+});
 
-    var Navigate = function(diff) {
-        displayBoxIndex += diff;
-        var oBoxCollection = $(".display_box");
-        if (displayBoxIndex >= oBoxCollection.length)
-            displayBoxIndex = 0;
-        if (displayBoxIndex < 0)
-            displayBoxIndex = oBoxCollection.length - 1;
-        var cssClass = "display_box_hover";
-        oBoxCollection.removeClass(cssClass).eq(displayBoxIndex).addClass(cssClass);
-    };
+var Navigate = function(diff) {
+    displayBoxIndex += diff;
+    var oBoxCollection = $(".display_box");
+    if (displayBoxIndex >= oBoxCollection.length)
+        displayBoxIndex = 0;
+    if (displayBoxIndex < 0)
+        displayBoxIndex = oBoxCollection.length - 1;
+    var cssClass = "display_box_hover";
+    oBoxCollection.removeClass(cssClass).eq(displayBoxIndex).addClass(cssClass);
+};
 
-    var selectListItem = function(diff) {
-        var oBoxCollection = $(".display_box");
-        var cssClass = "display_box_hover";
-        var temp = oBoxCollection.removeClass(cssClass).eq(displayBoxIndex).click();
+var selectListItem = function(diff) {
+    var oBoxCollection = $(".display_box");
+    var cssClass = "display_box_hover";
+    var temp = oBoxCollection.removeClass(cssClass).eq(displayBoxIndex).click();
 
-    };
+};
 
-    /*Autocompleter End*/
+/*Autocompleter End*/
 
-    $scope.getServiceName = function(serviceId){
-        var service = _.find($scope.services, function(service){ return service.id === serviceId;});
-        var response = (service !== undefined)? service.id: serviceId;
-        return response;
-    };
+$scope.getServiceName = function(serviceId){
+    var service = _.find($scope.services, function(service){ return service.id === serviceId;});
+    var response = (service !== undefined)? service.id: serviceId;
+    return response;
+};
 
 
 });
